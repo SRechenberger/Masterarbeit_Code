f4f9e8c37c9eec01e331755f1a968adc70691900
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-10-12 12:14:11 +0200

    initial commit

---
+++ src/solver/generic_solver.py
@@ -0,0 +1,57 @@
+from src.solver.utils import Formula, Assignment
+from src.experiment.utils import Measurement
+
+def generic_sls(
+        heuristic,
+        formula,
+        max_tries,
+        max_flips
+        init_context = lambda formula assignment: None,
+        update_context = lambda state variable: None,
+    ):
+    """ Generic SLS-Solver according to Algorithm 1,
+    including measurement facilities.
+    """
+
+    #initialize measurement object
+    measurement = Measurement(
+        formula.satisfying_assignment
+    )
+
+    t = 0
+    while t < max_tries:
+        # generate random assingnment
+        current_assignment = Assignment(
+            formula.number_of_variables
+        )
+        # setup measurement object for a search run
+        measurement.init_run(current_assignment)
+
+        # initialize context
+        context = init_context(formula, current_assignment)
+
+        f = 0
+        while f < max_flips:
+            # check, if the current assignment is a solution
+            if formula.is_satisfied(current_assignment):
+                return current_assignment, measurement
+
+            # choose variable to flip
+            to_flip = heuristic(
+                formula,
+                current_assignment,
+                context
+            )
+
+            # flip variable
+            current_assignment = current_assignment.flip(to_flip)
+
+            # update context
+            context = update_context(s, i)
+
+            # register flip in measurement object
+            measurement.count_flip(to_flip)
+
+    # If no solution is found,
+    # return None and the measurement object
+    return None, measurement
