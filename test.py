fe6e246f5ba0c14923304ca47257a95baf400d74
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-10-13 10:11:04 +0200

    tests for Falselist

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from src.solver.utils import Assignment
+from src.solver.utils import Assignment, Falselist
 
 class TestAssignment(unittest.TestCase):
     def setUp(self):
@@ -8,14 +8,14 @@ class TestAssignment(unittest.TestCase):
         self.maxDiff = None
 
     def test_creation(self):
-        for i in range(1,5000):
+        for i in range(1,1000):
             num_vars = random.randrange(1,i+1)
             assgn1 = Assignment.generate_random_assignment(num_vars)
             assgn2 = Assignment(int(str(assgn1),16),num_vars)
             self.assertEqual(str(assgn1),str(assgn2))
 
     def test_flip(self):
-        for i in range(1,5000):
+        for i in range(1,1000):
             num_vars = random.randrange(1,i+1)
             number   = random.randrange(0,pow(2,num_vars))
             assgn    = Assignment(number, num_vars)
@@ -24,6 +24,27 @@ class TestAssignment(unittest.TestCase):
             assgn.flip(to_flip)
             number ^= 1 << (to_flip-1)
             self.assertEqual(hex(number), str(assgn))
+
+
+class TestFalselist(unittest.TestCase):
+    def setUp(self):
+        random.seed()
+        self.maxDiff = None
+
+    def test_add(self):
+        l = Falselist()
+        t = set()
+        xs = list(range(1,1000))
+        random.shuffle(xs)
+        for x in xs:
+            l.add(x)
+            t.add(x)
+            self.assertEqual(set(l.lst),t)
+
+    def test_remove(self):
+        # TODO
+        pass
+
 
 
 if __name__ == '__main__':
