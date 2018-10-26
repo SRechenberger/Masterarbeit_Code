843f7e82c745b051dbf07f167446fe52eb75c358
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-10-26 11:40:03 +0200

    test creates its own test files

--- test/experiment/experiment.py
+++ test/experiment/experiment.py
@@ -7,6 +7,7 @@ from src.solver.generic_solver import Context
 from src.solver.gsat import gsat
 from src.solver.walksat import walksat
 from src.solver.probsat import probsat
+from src.solver.utils import Formula
 from src.utils import *
 
 class TestExperiment(unittest.TestCase):
@@ -14,10 +15,20 @@ class TestExperiment(unittest.TestCase):
     def setUp(self):
         random.seed()
         self.sample_size = 3
+        self.pool_dir = 'test_files'
+        number_formulae = 10
+        Formula.generate_formula_pool(
+            'test_files',
+            number_formulae,
+            256,
+            4.2,
+            poolsize = 3
+        )
+
 
     def test_creation(self):
         experiment = Experiment(
-            'testfiles',
+            self.pool_dir,
             self.sample_size,
             identity,
             1,1,Measurement
@@ -26,7 +37,7 @@ class TestExperiment(unittest.TestCase):
 
     def test_experiment_with_gsat(self):
         experiment = Experiment(
-            'testfiles',
+            self.pool_dir,
             self.sample_size,
             gsat,
             10,500,Measurement,
@@ -37,7 +48,7 @@ class TestExperiment(unittest.TestCase):
 
     def test_experiment_with_walksat(self):
         experiment = Experiment(
-            'testfiles',
+            self.pool_dir,
             self.sample_size,
             walksat,
             10,500,Measurement,
@@ -49,7 +60,7 @@ class TestExperiment(unittest.TestCase):
 
     def test_experiment_with_probsat(self):
         experiment = Experiment(
-            'testfiles',
+            self.pool_dir,
             self.sample_size,
             probsat,
             10,500,Measurement,
