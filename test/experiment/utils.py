2282561c94f5f9ec467c768a58f98d59a902f30d
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-11-01 14:36:40 +0100

    tests for src.experiment.utils

---
+++ test/experiment/utils.py
@@ -0,0 +1,62 @@
+from src.solver.utils import Formula
+from src.experiment.utils import EntropyMeasurement, FormulaSupply, binomial_vec
+import unittest
+import random
+import os
+from functools import partial
+
+class TestEntropyMeasurement(unittest.TestCase):
+    def setUp(self):
+        random.seed()
+        self.sample_size = 3
+        self.pool_dir = 'test_files'
+        number_formulae = 10
+        Formula.generate_formula_pool(
+            'test_files',
+            number_formulae,
+            10,
+            4.2,
+            poolsize = 3
+        )
+        self.dirs = random.sample(
+            list(
+                map(
+                    partial(os.path.join, self.pool_dir),
+                    os.listdir(self.pool_dir)
+                )
+            ),
+            10
+        )
+
+    def test_measure_tms_count(self):
+        for _,f in FormulaSupply(self.dirs):
+            m = EntropyMeasurement(f)
+            m.init_run(f.satisfying_assignment)
+            d = m.tms_steps
+            for i in range(1,f.num_vars+1):
+                m.count(i)
+
+            self.assertEqual(len(d),f.num_vars)
+            for (s1,s2),v in d.items():
+                    self.assertEqual(v,1)
+                    self.assertTrue(s1 == s2-1)
+
+            for i in range(f.num_vars,0,-1):
+                m.count(i)
+
+            self.assertFalse((0,0) in d)
+            self.assertFalse((f.num_vars,f.num_vars) in d)
+
+            self.assertEqual(len(d),2*f.num_vars)
+
+            for (s1,s2),v in d.items():
+                    self.assertEqual(v,1)
+                    self.assertTrue(s1 == s2-1 or s1 == s2+1)
+
+
+    def test_binomial_vec(self):
+        for i in range(1,1000):
+            self.assertTrue(abs(sum(binomial_vec(i)) - 1) < 0.001)
+
+
+
