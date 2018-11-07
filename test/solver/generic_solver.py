fdc980f5483421b056643ee5d15a3f4e05479196
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-11-07 21:49:52 +0100

    adapted generic tests accordingly

--- test/solver/generic_solver.py
+++ test/solver/generic_solver.py
@@ -22,10 +22,9 @@ class TestSolver(unittest.TestCase):
     def setUp(self):
         random.seed()
 
-        cases = 1 if __debug__ else 10
-        success_rate = 10
+        cases = 1 if __debug__ else 100
 
-        n = 128
+        n = 64
         r = 4.0
 
         formulae = [
@@ -34,12 +33,12 @@ class TestSolver(unittest.TestCase):
         ]
 
         self.solver_setup = dict(
-            max_flips     = n * 5,
+            max_flips     = n*5,
             max_tries     = 100,
             formulae      = formulae,
             max_run_time  = 20,
             cases         = cases,
-            min_successes = cases // 10,
+            min_successes = cases // 100,
         )
 
 
@@ -60,11 +59,12 @@ class TestSolver(unittest.TestCase):
             if measurement.get_run_time() >= self.solver_setup['max_run_time']:
                 run_time_exceeded += 1
 
+        print(successes)
         self.assertTrue(successes >= self.solver_setup['min_successes'])
-        if not __debug__:
-            self.assertTrue(
-                run_time_exceeded < self.solver_setup['cases'] - self.solver_setup['min_successes']
-            )
+        
+        self.assertTrue(
+            run_time_exceeded < self.solver_setup['cases'] - self.solver_setup['min_successes']
+        )
 
 
     def test_solver(self):
