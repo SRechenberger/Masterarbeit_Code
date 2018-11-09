4986248a87f05e28a9f1d5b1f0a9ee2d64977012
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-11-09 20:31:36 +0100

    fixed the bug (don't know how) [ci skip]

--- src/solver/utils.py
+++ src/solver/utils.py
@@ -1,278 +1,10 @@
-import platform
-import random
 import re
 import copy
 import os
 import sys
-import multiprocessing as mp
 from src.utils import *
-from collections.abc import Sequence
+from src.formula import Formula, Assignment
 
-if platform.sys.version_info.major < 3:
-    raise Exception("Must be Python 3")
-
-if platform.sys.version_info.minor < 6:
-
-    def choices(seq, weights = None):
-        if not weights:
-            weights = [1/len(seq) for _ in range(0,len(seq))]
-
-        acc = 0
-        dice = random.random()
-        for p,x in zip(weights, seq):
-            acc += p
-            if acc > dice:
-                return [x]
-
-    random.choices = choices
-
-
-
-class Formula:
-    """ CNF formulae in DIMACS format """
-
-    def __init__(self, dimacs = None, clauses = None, num_vars = None, sat_assignment = None):
-        """ Load a formula from a .cnf (DIMACS) file """
-        # check for argument validity
-        if __debug__:
-            type_check('dimacs', dimacs, str, optional = True)
-            instance_check('clauses', clauses, Sequence, optional = True)
-            type_check('num_vars', num_vars, int, optional = True)
-            value_check('num_vars', num_vars, optional = True, positive = strict_positive)
-            instance_check('sat_assignment', sat_assignment, Assignment, optional = True)
-
-        # init variables
-        self.clauses = []
-        self.num_clauses = 0
-        self.num_vars = 0
-        self.comments = []
-        self.occurrences = []
-        self.max_clause_length = 0
-        self.satisfying_assignment = None
-
-        # parse the file.
-        if dimacs:
-            r = re.compile(r'-?\d+')           # find numbers
-            rh = re.compile(r'-?0x[0-9a-fA-F]+') # find hex numbers
-
-            for line in dimacs.splitlines():
-                if line[0] == 'c':
-                    if line.startswith('c assgn'):
-                        hex_val, = rh.findall(line)
-                    else:
-                        self.comments.append(line)
-                elif line[0] == 'p':
-                    n, m = r.findall(line)
-                    self.num_vars = int(n)
-                    self.num_clauses = int(m)
-                    self.satisfying_assignment = Assignment(int(hex_val,16), int(n))
-                else:
-                    self.clauses.append(list(map(int, r.findall(line)))[:-1])
-                    if len(self.clauses[-1]) > self.max_clause_length:
-                        self.max_clause_length = len(self.clauses[-1])
-
-        # or use the given stuff
-        elif not dimacs and clauses and num_vars and sat_assignment:
-            self.satisfying_assignment = sat_assignment
-            self.clauses = clauses
-            self.num_clauses = len(clauses)
-            self.num_vars = num_vars
-            self.max_clause_length = max(map(len,clauses))
-
-        else:
-            raise ValueError(
-                "Either 'dimacs' or 'clauses', 'num_vars' and 'sat_assignment' must be provided"
-            )
-
-        self.occurrences = [[] for _ in range(0,self.num_vars*2+1)];
-
-        for clause_idx, clause in enumerate(self.clauses):
-            for lit in clause:
-                self.occurrences[self.num_vars + lit].append(clause_idx)
-
-        self.max_occs = 0
-        for occ in self.occurrences:
-            if len(occ) > self.max_occs:
-                self.max_occs = len(occ)
-
-        self.ratio = self.num_clauses / self.num_vars
-
-
-    def __eq__(self, formula):
-        if not formula:
-            return False
-        else:
-            return all([
-                self.num_vars == formula.num_vars,
-                self.clauses == formula.clauses,
-                str(self.satisfying_assignment) == str(formula.satisfying_assignment)
-            ])
-
-    def __str__(self):
-        """ Represent formula in DIMACS format """
-
-        toReturn = ''
-
-        for comment in self.comments:
-            toReturn += comment
-        toReturn += 'c assgn {}\n'.format(str(self.satisfying_assignment))
-        toReturn += 'p cnf {} {}\n'.format(self.num_vars, self.num_clauses)
-        for clause in self.clauses:
-            for literal in clause:
-                toReturn += '{} '.format(literal)
-            toReturn += '0\n'
-
-        return toReturn
-
-
-    def is_satisfied_by(self, assignment):
-        assert isinstance(assignment, Assignment), "assignment is no Assignment"
-
-        for clause in self.clauses:
-            true_clause = False
-            for literal in clause:
-                if assignment.is_true(literal):
-                    true_clause = True
-                    break
-            if not true_clause:
-                return False
-
-        return True
-
-
-    def get_occurrences(self, literal):
-        assert type(literal) == int, "literal is no int"
-        assert literal != 0, "literal == 0"
-
-        return self.occurrences[self.num_vars + literal]
-
-
-    def generate_satisfiable_formula(num_vars, ratio, clause_length = 3, seed=None):
-        assert type(clause_length) == int, "clause_length is no int"
-        assert clause_length > 0, "clause_length <= 0"
-        assert type(num_vars) == int, "num_vars is no int"
-        assert num_vars > 0, "num_vars <= 0"
-        assert type(ratio) == float, "ratio is no float"
-        assert ratio > 0, "ratio <= 0"
-        assert clause_length == 3, 'Method \'generate_satisfiable_formula\' only implemented for 3CNF yet.'
-
-        # optional arguments
-        if seed:
-            random.seed(seed)
-
-        satisfying_assignment = Assignment.generate_random_assignment(
-            num_vars,
-            seed = seed
-        )
-
-
-        num_clauses = int(ratio * num_vars)
-        clauses = []
-        p={1: 0.191, 2: 0.118, 3: 0.073}
-        for i in range(0, num_clauses + 1):
-            variables = random.sample(range(1,num_vars+1), clause_length)
-
-            acc = [[]]
-            for var in variables:
-                acc = list(map(lambda xs: [var] + xs, acc)) + list(map(lambda xs: [-var] + xs, acc))
-
-            f = lambda lit: 1 if satisfying_assignment.is_true(lit) else 0
-            cs, ws = [], []
-            for clause in acc:
-                x = sum(map(f,clause))
-                if x > 0:
-                    cs.append(clause)
-                    ws.append(p[x])
-
-            c, = random.choices(cs,weights=ws)
-            clauses.append(c)
-
-        formula = Formula(
-            clauses = clauses,
-            num_vars = num_vars,
-            sat_assignment = satisfying_assignment
-        )
-        if not formula.is_satisfied_by(satisfying_assignment):
-            raise RuntimeError('formula not satisfied')
-        return formula
-
-
-    def generate_formula_pool(
-            directory,
-            number,
-            num_vars,
-            ratio,
-            clause_length = 3,
-            seed = None,
-            poolsize = 1,
-            verbose = False):
-        assert type(directory) == str, "directory is no str"
-        assert type(number) == int, "number is no int"
-        assert number > 0, "number <= 0"
-        assert type(num_vars) == int, "num_vars is no int"
-        assert num_vars > 0, "num_vars <= 0"
-        assert type(ratio) == float, "ratio is no float"
-        assert ratio > 0, "ratio <= 0"
-        assert type(clause_length) == int, "clause_length is no int"
-        assert clause_length > 0, "clause_length <= 0"
-
-        if seed:
-            random.seed(seed)
-
-        try:
-            os.mkdir(directory)
-        except FileExistsError:
-            pass
-
-        with mp.Pool(processes = 3) as pool:
-            future_formulae = []
-            for _ in range(0,number):
-                ff = pool.apply_async(
-                    Formula.generate_satisfiable_formula,
-                    (
-                        num_vars,
-                        ratio,
-                        clause_length
-                    )
-                )
-                future_formulae.append(ff)
-
-            idx = 0
-            for ff in future_formulae:
-                f = ff.get()
-                filename = 'n{}-r{:.2f}-k{}-{:016X}.cnf'.format(
-                    num_vars,
-                    ratio,
-                    clause_length,
-                    hash(f),
-                )
-                with open(os.path.join(directory,filename),'w') as target:
-                    target.write(str(f))
-
-                del future_formulae[idx]
-                idx += 1
-
-                if verbose:
-                    print(
-                        'File {} written.'.format(
-                            os.path.join(directory,filename)
-                        )
-                    )
-
-
-
-    def __hash__(self):
-        return abs(
-            hash(
-                '{}{}{}{}{}'.format(
-                    self.num_vars,
-                    self.max_clause_length,
-                    self.ratio,
-                    str(self.satisfying_assignment),
-                    id(self)
-                )
-            )
-        )
 
 class Falselist:
     """ Models a list with no need for order,
@@ -306,119 +38,6 @@ class Falselist:
 
     def __len__(self):
         return len(self.lst)
-
-
-
-class Assignment:
-    """ Assignment modelled as an array of bits """
-
-    def generate_random_assignment(num_vars, seed = None):
-        """ Randomly generating a number between
-        0 and 2^num_vars, converting it into an assignment,
-        and returning it
-        """
-        if __debug__:
-            type_check('num_vars',num_vars,int)
-            value_check('num_vars',num_vars,strict_positive=strict_positive)
-
-        if seed:
-            random.seed(seed)
-
-        return Assignment(
-            random.randrange(0,pow(2,num_vars)),
-            num_vars,
-        )
-
-
-    def atoms_from_integer(number):
-        """ Takes a number and converts it into a list of booleans """
-        atoms = []
-        n = number
-        while n > 0:
-            atoms.append(n % 2 == 1)
-            n //= 2
-        return atoms
-
-
-    def integer_from_atoms(atoms):
-        """ Takes a list of booleans and converts it into a number """
-        n = 0
-        tmp = atoms.copy()
-        while tmp:
-            n *= 2
-            n += 1 if tmp.pop() else 0
-
-        return n
-
-
-    def flip(self, var_index):
-        """ Flips the variable with the index given """
-        if __debug__:
-            type_check('var_index',var_index,int)
-            value_check('var_index',var_index,strict_positive=strict_positive)
-
-        self.atoms[var_index-1] = not self.atoms[var_index-1]
-
-
-    def get_value(self, var_index):
-        """ Returns the assignment to the variable with the index given """
-        if __debug__:
-            type_check('var_index',var_index,int)
-            value_check(
-                'var_index',var_index,
-                strict_positive = strict_positive,
-                less_than_n = lambda x: x <= self.num_vars
-            )
-
-        return self.atoms[var_index-1]
-
-    def __getitem__(self, var_index):
-        return self.get_value(var_index)
-
-    def __setitem__(self, var_index, value):
-        self.atoms[var_index] = True if value else False
-
-    def __delitem__(self, var_index):
-        pass
-
-
-    def is_true(self, literal):
-        if __debug__:
-            type_check('literal',literal,int)
-            value_check('literal',literal,not_zero = not_equal_to(0))
-
-        t = self.get_value(abs(literal))
-        return t if literal > 0 else not t
-
-
-    def __init__(self, number, num_vars):
-        """ Generate an assignment from an integer """
-        if __debug__:
-            type_check('number',number,int)
-
-        self.atoms = Assignment.atoms_from_integer(number)
-        self.atoms += [False]*(num_vars-len(self.atoms))
-        self.num_vars = num_vars
-
-
-    def __str__(self):
-        """ Converts the assignment to a hex literal with 0x prefix """
-        return hex(Assignment.integer_from_atoms(self.atoms))
-
-
-    def hamming_dist(self, assgn):
-        if __debug__:
-            instance_check('assgn',assgn,Assignment)
-            value_check(
-                'assgn',assgn,
-                n_matches = lambda a: a.num_vars == self.num_vars
-            )
-
-        dist = 0
-        for i in range(0,self.num_vars):
-            dist += 1 if self[i] != assgn[i] else 0
-
-        return dist
 
 
 class DiffScores:
@@ -457,10 +76,11 @@ class DiffScores:
                 if self.num_true_lit[clause_idx] == 0:
                     makes += 1
 
-            if makes - breaks != self.get_score(var):
+            diff = (makes - breaks)
+            if diff != self.get_score(var):
                 print(
                     "Variable {}: make {} break {} diff {} desired {}".format(
-                        var, makes, breaks, makes - breaks, self.get_score(var)
+                        var, makes, breaks, diff, self.get_score(var)
                     ),
                     file = sys.stderr,
                 )
@@ -600,10 +220,10 @@ class DiffScores:
                 falselist.remove(clause_idx)
                 # no literal in the clause makes it sat anymore (for it is now sat),
                 # but 'variable' would break it,
-                # therefore, its score does not change
+                # therefore, its score is decremented twice
+                self.decrement_score(variable)
                 for lit in formula.clauses[clause_idx]:
-                    if abs(lit) != variable:
-                        self.decrement_score(abs(lit))
+                    self.decrement_score(abs(lit))
                 # make 'variable' critical (for it is now the only satisfying variable)
                 self.crit_var[clause_idx] = variable
 
@@ -623,11 +243,11 @@ class DiffScores:
                 # add the clause to the falselist
                 falselist.add(clause_idx)
                 # 'variable' now no longer breaks the clause, but would make it;
-                # thus its score does not change
+                # thus its score is incremented twice
                 # the score of all other variables will be increased
+                self.increment_score(variable)
                 for lit in formula.clauses[clause_idx]:
-                    if abs(lit) != variable:
-                        self.increment_score(abs(lit))
+                    self.increment_score(abs(lit))
                 # set variable as critical (for whatever reason)
                 self.crit_var[clause_idx] = variable
 
--- test/solver/utils.py
+++ test/solver/utils.py
@@ -2,43 +2,9 @@ import unittest
 import random
 import os
 
-from src.solver.utils import Assignment, Falselist, Formula, Scores, DiffScores
+from src.formula import Formula, Assignment
+from src.solver.utils import Falselist, Scores, DiffScores
 from src.experiment.utils import FormulaSupply
-
-class TestAssignment(unittest.TestCase):
-    def setUp(self):
-        random.seed()
-        self.maxDiff = None
-
-
-    def test_creation(self):
-        for i in range(1,1000):
-            num_vars = random.randrange(1,i+1)
-            assgn1 = Assignment.generate_random_assignment(num_vars)
-            assgn2 = Assignment(int(str(assgn1),16),num_vars)
-            self.assertEqual(str(assgn1),str(assgn2))
-
-
-    def test_flip(self):
-        for i in range(1,1000):
-            num_vars = random.randrange(1,i+1)
-            number   = random.randrange(0,pow(2,num_vars))
-            assgn    = Assignment(number, num_vars)
-            self.assertEqual(hex(number), str(assgn))
-            to_flip  = random.randrange(1,num_vars+1)
-            assgn.flip(to_flip)
-            number ^= 1 << (to_flip-1)
-            self.assertEqual(hex(number), str(assgn))
-
-
-    def test_index(self):
-        for i in range(1,1000):
-            num_vars = random.randrange(1,i+1)
-            assgn    = Assignment.generate_random_assignment(num_vars)
-            to_flip  = random.randrange(1,num_vars+1)
-            old      = assgn[to_flip]
-            assgn.flip(to_flip)
-            self.assertNotEqual(old, assgn[to_flip])
 
 
 class TestFalselist(unittest.TestCase):
@@ -79,15 +45,20 @@ class TestScores(unittest.TestCase):
             5,
             256,
             4.2,
-            poolsize=3
+            poolsize=3,
         )
-        self.paths = list(map(
-            lambda fname: os.path.join(dirname, fname),
-            filter(
-                lambda fname: fname.endswith('.cnf'),
-                os.listdir(dirname)
-            )
-        ))
+        self.paths = random.sample(
+            list(
+                map(
+                    lambda fname: os.path.join(dirname, fname),
+                    filter(
+                        lambda fname: fname.endswith('.cnf'),
+                        os.listdir(dirname)
+                    )
+                )
+            ),
+            10
+        )
         self.buffsize = 5
 
 
@@ -102,16 +73,20 @@ class TestScores(unittest.TestCase):
 
             to_flips = [
                 i
-                for _ in range(0,100)
-                for i in random.sample(range(1,n+1), n // 2)
+                for _ in range(0,10)
+                for i in random.sample(range(1,n+1), n // 10)
             ]
 
-            for to_flip in to_flips:
-                diff_score.flip(to_flip, formula, assgn, falselist)
+            print("score({}) = {}".format(1,diff_score.get_score(1)))
+            diff_score.flip(1, formula, assgn, falselist)
+            #for to_flip in to_flips:
+            #    diff_score.flip(to_flip, formula, assgn, falselist)
+            #    print(to_flip)
+            print("score({}) = {}".format(1,diff_score.get_score(1)))
 
-                self.assertTrue(diff_score.self_test(formula, assgn, falselist))
+            self.assertTrue(diff_score.self_test(formula, assgn, falselist))
 
-
+    # @unittest.skip("takes too long")
     def test_score(self):
         for _, formula in FormulaSupply(self.paths, self.buffsize):
             n = formula.num_vars
@@ -134,117 +109,14 @@ class TestScores(unittest.TestCase):
             # first check after creation
             self.assertTrue(scores.self_test(formula, assgn, falselist))
 
-            # flipping back and forth leave everything as is
+            to_flips = [
+                i
+                for _ in range(0,10)
+                for i in random.sample(range(1,n+1), max(1,n // 100))
+            ]
 
-            ## from True to False and back
-            for pos in pos_flips:
-                old_breaks = scores.breaks.copy()
-                scores.flip(pos, formula, assgn, falselist)
-                self.assertTrue(
-                    scores.self_test(formula,assgn,falselist)
-                )
-                self.assertNotEqual(
-                    old_breaks,
-                    scores.breaks
-                )
-                scores.flip(pos, formula, assgn, falselist)
-                self.assertEqual(
-                    old_breaks,
-                    scores.breaks
-                )
-                self.assertTrue(
-                    scores.self_test(formula,assgn,falselist)
-                )
+            for to_flip in to_flips:
+                scores.flip(to_flip, formula, assgn, falselist)
 
-            ## from False to True and back
-            for neg in neg_flips:
-                old_breaks = scores.breaks.copy()
-                scores.flip(neg, formula, assgn, falselist)
-                self.assertTrue(
-                    scores.self_test(formula,assgn,falselist)
-                )
-                scores.flip(neg, formula, assgn, falselist)
-                self.assertEqual(
-                    old_breaks,
-                    scores.breaks
-                )
-                self.assertTrue(
-                    scores.self_test(formula,assgn,falselist)
-                )
+                self.assertTrue(scores.self_test(formula, assgn, falselist))
 
-
-class TestFormula(unittest.TestCase):
-    def setUp(self):
-        random.seed()
-        self.cases = 10 if __debug__ else 500
-
-
-    def test_random_creation_and_reading(self):
-        for i in range(0,self.cases):
-            f = Formula.generate_satisfiable_formula(500, 4.2)
-            self.assertEqual(f, Formula(dimacs = str(f)))
-
-
-    def test_occurrence_counting(self):
-        for i in range(0,self.cases):
-            f = Formula.generate_satisfiable_formula(500, 4.2)
-            occ_count = {}
-            for clause in f.clauses:
-                for lit in clause:
-                    if lit in occ_count:
-                        occ_count[lit] += 1
-                    else:
-                        occ_count[lit] = 1
-
-            for k,v in occ_count.items():
-                self.assertEqual(
-                    len(f.get_occurrences(k)),
-                    v
-                )
-                self.assertTrue(
-                    v <= f.max_occs
-                )
-
-    def test_occurrences(self):
-        for i in range(0,self.cases):
-            f = Formula.generate_satisfiable_formula(500,4.2)
-            for i in range(1,501):
-                pos_occs = f.get_occurrences(i)
-                neg_occs = f.get_occurrences(-i)
-
-                # validate positive occurrences
-                for clause_idx in pos_occs:
-                    self.assertTrue(i in f.clauses[clause_idx])
-
-                # validate negative occurrences
-                for clause_idx in neg_occs:
-                    self.assertTrue(-i in f.clauses[clause_idx])
-
-
-    def test_random_creation(self):
-        for i in range(0,self.cases):
-            n = random.randrange(10,1001)
-            r = random.randrange(20,42)/10
-            f = Formula.generate_satisfiable_formula(n, r)
-            self.assertTrue(abs(f.num_clauses - n * r) < 2)
-
-
-    def test_satisfiable_assignment(self):
-        for i in range(0,self.cases):
-            n = random.randrange(10,1001)
-            r = random.randrange(20,42)/10
-            f = Formula.generate_satisfiable_formula(n,r)
-            self.assertTrue(f.is_satisfied_by(f.satisfying_assignment))
-
-
-    def test_hardness(self):
-        n = 500
-        for i in range(0,self.cases):
-            f = Formula.generate_satisfiable_formula(n, 4.2)
-            atoms = 0
-            for var in range(1,n+1):
-                if len(f.get_occurrences(var)) > len(f.get_occurrences(-var)):
-                    atoms += 1
-                atoms *= 2
-            a = Assignment(atoms, n)
-            self.assertFalse(f.is_satisfied_by(a))
