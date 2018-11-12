3865891b4f6d7fb5fd0b9fd99b24f2d94dda3026
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-11-12 15:06:27 +0100

    tests for Queue

--- test/experiment/utils.py
+++ test/experiment/utils.py
@@ -1,9 +1,52 @@
 from src.solver.utils import Formula
-from src.experiment.utils import EntropyMeasurement, FormulaSupply, binomial_vec
+from src.experiment.utils import EntropyMeasurement, FormulaSupply, binomial_vec, Queue
 import unittest
 import random
 import os
 from functools import partial
+
+
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        random.seed()
+        self.cases = 100
+
+    def test_push_read(self):
+        for _ in range(0, self.cases):
+            size = random.randrange(0,1000) + 1
+
+            test_queue = Queue(size)
+
+            xs = random.sample(range(1,1001), size)
+
+            for x in xs:
+                test_queue.push(x)
+
+            ys = []
+            while not test_queue.is_empty():
+                ys.append(test_queue.pop())
+
+            self.assertEqual(xs,ys)
+
+
+    def test_over_fill(self):
+        for _ in range(0, self.cases):
+            size = random.randrange(0,1000) + 1
+
+            test_queue = Queue(size)
+
+            xs = random.sample(range(1,1001), size)
+            ys = random.sample(range(1,1001), size)
+
+            for x in xs:
+                test_queue.push(x)
+
+            zs = []
+            for y in ys:
+                zs.append(test_queue.push(y))
+
+            self.assertEqual(xs,zs)
+
 
 class TestEntropyMeasurement(unittest.TestCase):
     def setUp(self):
