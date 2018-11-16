6acc1be097a86856f45b80416fddd26d8ec2a949
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-11-16 18:49:40 +0100

    distribution test for probsat

--- test/solver/probsat.py
+++ test/solver/probsat.py
@@ -1,6 +1,15 @@
-from test.solver.generic_solver import TestSolver
-from src.solver.probsat import probsat
+from test.solver.generic_solver import TestSolver, TestDistribution
+from src.solver.probsat import probsat, probsat_heuristic, probsat_distribution
+from src.solver.walksat import DefensiveContext
 from functools import partial
+
+class TestWalkSATDistr(TestDistribution):
+    def test_distr(self):
+        self.generic_test_distribution_against_heuristic(
+            DefensiveContext,
+            probsat_heuristic(int(self.n * self.r), 2.3),
+            probsat_distribution(int(self.n * self.r), 2.3),
+        )
 
 class TestProbSAT(TestSolver):
     def test_solver(self):
