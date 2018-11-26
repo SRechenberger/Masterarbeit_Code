1acd507388614da650d4b6db76c1bc5dc6d34f37
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-11-26 14:47:40 +0100

    names

--- jobs/test/walksat/static_n512_100_test.sh
+++ jobs/test/walksat/static_n512_100_test.sh
@@ -2,7 +2,7 @@
 #MSUB -l nodes=1:ppn=16
 #MSUB -l walltime=1:00:00
 #MSUB -l pmem=3000mb
-#MSUB -N gsat_n512_100samples_test
+#MSUB -N walksat-static-n512-100-test
 #MSUB -o /work/ul/ul_student/ul_pwn14/output/gsat_n256_100samples_test.txt
 #MSUB -M sascha.rechenberger@uni-ulm.de
 #MSUB -m bea
