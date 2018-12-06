41a720892f179f61c8da6e02d3a2f6131da43b10
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-12-06 21:02:42 +0100

    blub

--- jobs/static_experiment_1/static_experiment_1.sh
+++ jobs/static_experiment_1/static_experiment_1.sh
@@ -1,16 +1,18 @@
 NAME=static_experiment_1
+WALKSAT="0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0"
+PROBSAT="0.0 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0"
 
 # GSAT
 sh ../gsat.static.template.sh 256 $NAME
 
 # WalkSAT
-for i in $(cat walksat_parameters);
+for i in $WALKSAT;
 do
   sh ../walksat.static.template.sh 256 $i $((NAME))_rho$i;
 done;
 
 # ProbSAT
-for i in $(cat probsat_parameters);
+for i in $PROBSAT;
 do
   sh ../probsat.static.template.sh 256 $i $((NAME))_cb$i;
 done;
