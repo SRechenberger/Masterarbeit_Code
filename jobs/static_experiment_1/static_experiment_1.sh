652faae3d2754654657af44e650c77a3331e9a6c
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-12-07 11:20:57 +0100

    ...

--- jobs/static_experiment_1/static_experiment_1.sh
+++ jobs/static_experiment_1/static_experiment_1.sh
@@ -4,17 +4,17 @@ PROBSAT="0.0 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4
 N=512
 
 # GSAT
-msub jobs/gsat.static.template.sh 512 $NAME
+msub jobs/gsat.static.template.sh $N $NAME
 
 # WalkSAT
 for i in $WALKSAT;
 do
-  msub jobs/walksat.static.template.sh 512 $i `echo $NAME`_rho$i;
+  msub jobs/walksat.static.template.sh $N $i `echo $NAME`_rho$i;
 done;
 
 # ProbSAT
 for i in $PROBSAT;
 do
-  msub jobs/probsat.static.template.sh 512 $i `echo $NAME`_cb$i;
+  msub jobs/probsat.static.template.sh $N $i `echo $NAME`_cb$i;
 done;
 
