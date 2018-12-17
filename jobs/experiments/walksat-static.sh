2eb42631ac556cfbb31bb2d575da14ae52cdda1d
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-12-17 18:48:39 +0100

    Update walksat-static.sh

--- jobs/experiments/walksat-static.sh
+++ jobs/experiments/walksat-static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #MSUB -l nodes=1:ppn=16
-#MSUB -l walltime=17:00:00
+#MSUB -l walltime=19:00:00
 #MSUB -l pmem=3000mb
 #MSUB -M sascha.rechenberger@uni-ulm.de
 #MSUB -m bea
@@ -14,5 +14,5 @@ python -O run_experiment.py $WORK/input/n512 \
   --static \
   --walksat $RHO \
   --poolsize 16 \
-  --database_file $WORK/output/data/walksat-rho`echo $RHO`.db \
+  --database_file $WORK/output/data/walksat-`echo $RHO`.db \
   --verbose
