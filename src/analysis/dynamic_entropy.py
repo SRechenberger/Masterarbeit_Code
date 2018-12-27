e0216f20e8f6390410c4a7ec86442a33771c16be
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2018-12-27 15:23:04 +0100

    modified query

--- src/analysis/dynamic_entropy.py
+++ src/analysis/dynamic_entropy.py
@@ -7,21 +7,63 @@ import numpy as np
 
 from functools import partial
 
-def path_entropy_to_performance(file, entropy, field):
-    with sqlite3.connect(file) as conn:
-        rows = conn.cursor().execute(
-            f""" SELECT formula_id, {field}, sat, total_runtime \
-            FROM algorithm_run NATURAL JOIN search_run JOIN entropy_data ON {entropy} = data_id \
-            """
-        )
-        results = []
-        for f_id, avg_h, avg_sat, avg_rt in rows:
-            results.append((f_id, avg_h, avg_sat, avg_rt))
+def path_entropy_to_runtime(folder, field, verbose=False):
+    results = []
+    files = map(partial(os.path.join, folder), os.listdir(folder))
+    for file in files:
+        try:
+            with sqlite3.connect(file) as conn:
+                rows = conn.cursor().execute(
+                    f"""
+                    WITH entropies AS
+                        (SELECT
+                            run_id,
+                            formula_id,
+                            sat,
+                            total_runtime,
+                            avg(h1.{field}) as h1,
+                            avg(h2.{field}) as h2,
+                            avg(h3.{field}) as hc,
+                            avg(h4.{field}) as i
+                        FROM algorithm_run
+                            NATURAL JOIN search_run
+                            JOIN entropy_data h1 ON single_entropy = h1.data_id
+                            JOIN entropy_data h2 ON joint_entropy = h2.data_id
+                            JOIN entropy_data h3 ON cond_entropy = h3.data_id
+                            JOIN entropy_data h4 ON mutual_information = h4.data_id
+                        GROUP BY
+                            run_id)
+                    SELECT
+                        formula_id,
+                        avg(sat),
+                        avg(total_runtime),
+                        avg(h1),
+                        avg(h2),
+                        avg(hc),
+                        avg(i)
+                    FROM entropies
+                    GROUP BY formula_id
+                    """
+                )
 
-        return pandas.DataFrame.from_records(
-            results,
-            columns=['formula_id', 'value', 'avg_sat', 'avg_runtime']
-        )
+        except sqlite3.OperationalError as e:
+            print(f'Skipped file {file} because of: {e}', file=sys.stderr)
+            continue
+
+        for f_id, avg_sat, avg_rt, *avg_hs in rows:
+            results.append((f_id, max(1, 10 * (1-avg_sat) * avg_rt), *avg_hs))
+
+    return pandas.DataFrame.from_records(
+        results,
+        columns=[
+            'formula_id',
+            'runtime',
+            'single_entropy',
+            'joint_entropy',
+            'cond_entropy',
+            'mutual_information'
+        ]
+    )
 
 
 def noise_to_performance(folder, verbose=False):
